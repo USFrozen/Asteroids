fba6fbe7d9c1261d5d446262a66ef8f84b7a012a
USFrozen <acefrozen1@gmail.com>
2025-07-18 15:55:03 -0500

    added code comments to main.py

--- main.py
+++ main.py
@@ -2,25 +2,50 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+# Imports game constants from local constants.py
 from constants import *
 
 def main():
+    # Startup messages, shows game resolution
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # Initalizes pygame
     pygame.init()
+
+    # Sets pygame display to use SCREEN_WIDTH and SCREEN_HEIGHT from constants.py
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Sets pygame clock, used for game tick function
     clock = pygame.time.Clock()
-    dt = 0
+
+    # Sets game FPS limit to 60 to limit resource usage. Pauses game tick for 1/60th of a second
     fps = 60
+
+    # Delta Time, time since last tick happened in miliseconds
+    dt = 0
+
+    # Primary game loop
+    # Allows quitting game through X button, Alt F4, and other methods
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        # Fills screen with black color
         screen.fill((0,0,0))
+
+        # Flips frame buffer to next frame (default 60 FPS)
         pygame.display.flip()
+
+        # Draw player
+        #
+
+        # Ticks game loop, pauses for (default) 1/60th of a second, sets Delta Time in MS
         dt = (clock.tick(fps) / 1000)
 
+# Makes it so main loop doesnt run if main.py is imported elsewhere
 if __name__ == "__main__":
     main()
 
